a333ad572242ee583975e65df83e3a7cece5f778
mosecoza <mosecoza@gmail.com>
2016-09-27 14:50:17 +0200

    -fixed the layout theme -added a countdown timer -removed background images

--- app/src/main/java/za/co/example/bonginkosilukhele/courtcounter/MainActivity.java
+++ app/src/main/java/za/co/example/bonginkosilukhele/courtcounter/MainActivity.java
@@ -1,19 +1,44 @@
 package za.co.example.bonginkosilukhele.courtcounter;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
     int home_score=0;
     int away_score=0;
+    private static final String FORMAT = "%02d:%02d:%02d";
+    TextView text1;
+    int seconds , minutes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text1=(TextView)findViewById(R.id.tv_timer);
+        new CountDownTimer(1234000, 300) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                text1.setText(""+String.format(FORMAT,
+                        TimeUnit.MILLISECONDS.toHours(millisUntilFinished),
+                        TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished) - TimeUnit.HOURS.toMinutes(
+                                TimeUnit.MILLISECONDS.toHours(millisUntilFinished)),
+                        TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) - TimeUnit.MINUTES.toSeconds(
+                                TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished))));
+
+            }
+
+            @Override
+            public void onFinish() {
+                text1.setText("done!");
+            } // adjust the milli seconds here
+        }.start();
     }
 
         public void add3Home(View view){
@@ -54,4 +79,6 @@ public class MainActivity extends AppCompatActivity {
         displayHomeScore(0);
         displayAwayScore(0);
     }
+
+
 }
